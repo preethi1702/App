7264e4e35d7c6c8730c3a3d10cc014188af4855c
preethi <matampreethi1@gmail.com>
2020-05-18 21:25:38 +0530

    Covid

--- app/src/main/java/com/example/myfirstimage/MainActivity.kt
+++ app/src/main/java/com/example/myfirstimage/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.myfirstimage
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
     }
 }
